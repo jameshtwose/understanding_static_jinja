9ab736753c0cf4fcede0900dfe0786da4cc6f194
jameshtwose <jameshtwose@users.noreply.github.com>
2022-04-08 07:57:39 +0000

    deploy: f3012144d899a59075e59dc3d33f2bdbbd04a262

--- build_index.py
+++ build_index.py
@@ -2,14 +2,14 @@
 from glob import glob
 from contextlib import redirect_stdout
 
-from index_html_helpers import start_doc, end_doc, start_doc_index, navbar_start, navbar_end, dropdown_example, dropdown_begin
+from index_html_helpers import start_doc, end_doc, navbar_start, navbar_end, dropdown_example, dropdown_begin
 
 #%%
 file_name="index.html"
 with open(file_name, "w") as f:
     with redirect_stdout(f):
         title_string="Covid Data"
-        start_doc_index(title_string = title_string)       
+        start_doc(title_string = title_string)       
         print("<section id='mainbody'>")
         print(f"<h1>{title_string}</h1>")
         print("<ul>")
@@ -24,12 +24,6 @@ with open(file_name, "w") as f:
         print("</section>")
 
         navbar_start()
-        # for covid_file in sorted(glob("output_files/*")):
-        #     list_element = f"""<li class="nav-item">
-        #                 <a class="nav-link" href="{covid_file}">{covid_file[13:-5]}</a>
-        #             </li>
-        #             """
-        #     print(list_element)
         
         dropdown_begin()
         for covid_file in sorted(glob("output_files/*")):
